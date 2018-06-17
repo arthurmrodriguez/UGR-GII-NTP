087e8e069119519db31a75346f18893b99fc999c
arthurmrodriguez <amrn2404@gmail.com>
2018-06-17 13:27:34 +0200

    Clase Asignacion y pruebas completas al 100% :fire:

---
+++ PracticaFinal/src/main/scala/Asignacion.scala
@@ -0,0 +1,150 @@
+/**
+  * Clase Asignacion, compuesta por un dominio
+  * y una lista de valores para las variables
+  * @param dominio
+  * @param valores
+  */
+class Asignacion(val dominio : Dominio, val valores : List[Int]) {
+
+  val datos =  asignarValoresADominio
+
+  /**
+    * Metodo para comprobar si se define una
+    * Asignacion sobre un dominio vacio
+    * @return Asignacion vacia
+    */
+  def vacia : Boolean = dominio.vacio
+
+  /**
+    * Metodo para obtener el numero de variables
+    * que define la asignacion
+    * @return numero de variables del dominio
+    */
+  def obtenerNumeroVariables : Int = dominio.longitud
+
+
+  /**
+    * Metodo para obtener el valor de una variable
+    * en la asignacion.
+    * @param variable para obtener su valor
+    * @return valor de la variable, -1 en caso de que
+    *         esa variable no este en esa asignacion
+    */
+  def obtenerValorVariable(variable : Variable) : Int = datos.getOrElse(variable, -1)
+
+
+  /**
+    * Metodo para añadir un par variable-valor a la
+    * asignacion
+    * @param variable nueva
+    * @param valor de la variable
+    * @return Asignacion nueva
+    */
+  def +(variable : Variable, valor : Int) : Asignacion = {
+    if (dominio.variables.contains(variable))
+      this
+    else
+      Asignacion(dominio + variable, valores :+ valor)
+  }
+
+  /**
+    * Metodo para calcular el indice de la asignacion
+    * @return indice de la Asignacion
+    */
+  def calcularIndice : Int = {
+    (dominio.pesos zip datos.values).map{case (peso,valor) => peso*valor}.sum
+  }
+
+  /**
+    * Metodo que devuelve una Asignacion con las variables
+    * del dominio que se encuntran en SU dominio
+    * @param unDominio que contiene las variables interseccion
+    * @return nueva Asignacion
+    */
+  def proyectar(unDominio : Dominio) : Asignacion = {
+
+    val mDominio = Dominio(unDominio.variables.intersect(dominio.variables))
+    val mValores = mDominio.variables.map(variable => obtenerValorVariable(variable))
+
+    Asignacion(mDominio,mValores)
+
+  }
+
+  /**
+    * Metodo para mostrar por pantalla una asignacion
+    * @return Asignacion con formato adecuado
+    */
+  override def toString: String = {
+    if(datos != null)
+      datos.map{case (variable,valor) => "["+variable.nombre + " - "+valor +"]"} mkString " "
+    else
+      null
+  }
+
+  private def asignarValoresADominio : Map[Variable,Int] = {
+
+    if(valores.length == dominio.longitud){
+
+      dominio.variables.zip(valores).map{
+        case (variable,valor) =>
+
+          if(valor < 0 || valor > variable.numEstados)
+            return null
+          else
+            variable->valor
+
+      }.toMap
+
+    }
+
+    else
+      null
+
+  }
+
+}
+
+object Asignacion{
+  /**
+    * Metodo para crear una Asignacion a partir de un
+    * dominio y un conjunto de valores
+    * @param dominio correspondiente
+    * @param valores correspondientes
+    * @return nueva Asignacion
+    */
+  def apply(dominio : Dominio, valores : List[Int]) : Asignacion = {
+    new Asignacion(dominio,valores)
+  }
+
+  /**
+    * Metodo para crear una Asignacion unicamente
+    * a partir de un dominio, con valores 0 para cada
+    * una de las variables
+    * @param dominio correspondientes
+    * @return nueva Asignacion
+    */
+  def apply(dominio : Dominio) : Asignacion= {
+    new Asignacion(dominio, List.fill(dominio.longitud)(0))
+  }
+
+  /**
+    * Metodo para crear una Asignacion a partir de un
+    * dominio y un indice
+    * @param dominio correspondiente
+    * @param indice correspondiente
+    * @return nueva Asignacion
+    */
+  def apply(dominio : Dominio, indice : Int) : Asignacion = {
+
+    if(indice > dominio.maximoIndice)
+      Asignacion(dominio)
+
+    else{
+      val valoresIndice = dominio.indiceVariables.map{case(variable, index) =>
+        (indice/dominio.pesos(index)) % variable.numEstados
+      }.toList
+
+      new Asignacion(dominio,valoresIndice)
+    }
+  }
+}
--- PracticaFinal/src/main/scala/MainPrueba.scala
+++ PracticaFinal/src/main/scala/MainPrueba.scala
@@ -50,4 +50,44 @@ object MainPrueba extends App{
   val dominioResta2 = dominioSuma2 - dominioNoVacio
   println("Dominio resta dominioNoVacio2 " + dominioResta2)
 
+
+  // Se crea asignacion vacia y se comprueba su chequeo
+  val asignacionVacia = Asignacion(Dominio(List()), List())
+  println("Comprobacion vacio asignacion vacia: "+asignacionVacia.vacia)
+
+  // Se crea asignacion , dando valores 2, 3, 1 y 0 a las variables
+  val asignacion1 = Asignacion(Dominio(List(X1, X2,X3, X4)),List(2,3,1,0))
+  println("Comprobacion vacio sobre asignacion no vacia: " + asignacion1.vacia)
+  println("Se muestra la asignacion: ")
+  println(asignacion1)
+
+  // Obtener valor de la variable
+  val valorVariable = asignacion1.obtenerValorVariable(X4)
+  val valorVariableNE = asignacion1.obtenerValorVariable(X2)
+
+  println("Valor de la variable var4 en la asignacion1: " + valorVariable)
+  println("Valor de la variable X2 en la asignacion1: " + valorVariableNE)
+
+  // Añadir un par variable valor a la asignacion
+  val asignacion2 = asignacion1 + (X6,0)
+  println("Asignacion2 con par (X6,2) añadido: " + asignacion2)
+
+  // Calculo del indice asociado a la asignacion
+  val indice1=asignacion1.calcularIndice
+  println("indice1 (debe ser 46): " + indice1)
+
+  // Comprobar proyeccion
+  val proyeccion1 = asignacion2.proyectar(Dominio(List(X3,X4)))
+  println("Comprobar proyeccion sobre X3 y X4: "+ proyeccion1)
+
+  // Comprobar creacion con solo el dominio
+  val asignacion3 = Asignacion(dominioNoVacio)
+  println("Asignacion con valores 0: " + asignacion3)
+
+  // A partir del indice obtenemos la asignacion
+  val asignacionDeIndice = Asignacion(asignacion1.dominio, indice1)
+
+  // Se muestra la asignacion obtenida: debe ser X1=2, X2=3, X3=1, X4=0
+  println("Asignacion resultante a traves de un indice: " + asignacionDeIndice)
+
 }
